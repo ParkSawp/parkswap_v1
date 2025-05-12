1479fc0c878fca68b7a6b8db652352a6d348de5e
DIARRA Mamadou <afrocodeur@afrocodeur.local>
2025-05-12 08:02:34 +0000

    feat/portfolio-ui

---
+++ src/components/Portfolio/PortfolioPerformanceChart/PortfolioPerformanceChart.js
@@ -0,0 +1,46 @@
+
+import Canvas from "@canvasjs/react-charts";
+
+export default function PortfolioPerformanceChart({ }) {
+    const options = {
+        theme: "light",
+        animationEnabled: true,
+        exportEnabled: false,
+        // title: {
+        //     text: "Number of iPhones Sold"
+        // },
+        // axisY: {
+        //     title: "Number of iPhones ( in Million )"
+        // },
+        axisX: {
+            lineDashType: "dot",
+            lineThickness: 2
+        },
+        axisY: {
+            gridThickness: 0
+        },
+        data: [
+            {
+                type: "area",
+                xValueFormatString: "YYYY",
+                yValueFormatString: "#,##0.## Million",
+                dataPoints: [
+                    { x: new Date(2017, 0), y: 7.6},
+                    { x: new Date(2016, 0), y: 7.3},
+                    { x: new Date(2015, 0), y: 6.4},
+                    { x: new Date(2014, 0), y: 5.3},
+                    { x: new Date(2013, 0), y: 4.5},
+                    { x: new Date(2012, 0), y: 3.8},
+                    { x: new Date(2011, 0), y: 3.2}
+                ]
+            }
+        ]
+    }
+    return (
+        <div>
+            <Canvas.CanvasJSChart options = {options}
+                /* onRef={ref => this.chart = ref} */
+            />
+        </div>
+    );
+}
--- src/components/Portfolio/PortfolioWallet/PortfolioWallet.js
+++ src/components/Portfolio/PortfolioWallet/PortfolioWallet.js
@@ -4,6 +4,9 @@ import Translate from "@/src/components/Translate/Translate";
 import Amount from "@/src/components/Global/Amount/Amount";
 import {ChartIcon} from "@/src/components/Icon/Icon";
 import {useTranslation} from "react-i18next";
+import dynamic from "next/dynamic";
+
+const PortfolioPerformanceChart = dynamic(() => import("@/src/components/Portfolio/PortfolioPerformanceChart/PortfolioPerformanceChart"), { ssr: false });
 
 export default function PortfolioWallet({ }) {
 
@@ -12,73 +15,111 @@ export default function PortfolioWallet({ }) {
     const tokens = [
         {
             name: 'ETH',
-            icon: '',
+            icon: '/svg/icons/eth_icon.svg',
             type: 'native',
             balance: 22,
             price: 667
         },
         {
             name: 'USDT',
-            icon: '',
+            icon: '/svg/icons/pepe-pepe-logo.svg',
             type: 'stable',
             balance: 1800,
             price: 1
         },
         {
             name: 'USDC',
-            icon: '',
+            icon: '/svg/icons/ltc-logo.svg',
             type: 'stable',
             balance: 2700,
             price: 1
         },
         {
             name: 'UNI',
-            icon: '',
+            icon: '/svg/icons/dai-logo.svg',
             type: 'token',
             balance: 550,
             price: 12
+        },
+        {
+            name: 'SOL',
+            icon: '/svg/icons/sol-logo.svg',
+            type: 'token',
+            balance: 23,
+            price: 125
+        },
+        {
+            name: 'USDT',
+            icon: '/svg/icons/theter-logo.svg',
+            type: 'token',
+            balance: 8230,
+            price: 1
         }
     ];
 
     return (
         <>
             <div className={styles["portfolio-wallet-container-body"]}>
-                <div className={styles["portfolio-wallet-stats-container-body"]}>
-                    <div className={styles["portfolio-wallet-stats-item"]}>
-                        <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Value</Translate></div>
-                        <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={2230240}/></div>
-                    </div>
-                    <div className={styles["portfolio-wallet-stats-item"]}>
-                        <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Native Value</Translate></div>
-                        <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={1230240}/></div>
-                    </div>
-                    <div className={styles["portfolio-wallet-stats-item"]}>
-                        <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Stable Value</Translate></div>
-                        <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={230240}/></div>
-                    </div>
-                    <div className={styles["portfolio-wallet-stats-item"]}>
-                        <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Token Value</Translate></div>
-                        <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={730240}/></div>
+                <div class={styles['portfolio-section']} >
+                    <div className={styles['portfolio-wallet-section-title']}><Translate>Performance</Translate></div>
+                    <PortfolioPerformanceChart />
+                </div>
+                <div class={styles['portfolio-section']} >
+                    <div className={styles['portfolio-wallet-section-title']}><Translate>Performance</Translate></div>
+                    <div className={styles["portfolio-wallet-stats-container-body"]}>
+                        <div className={styles["portfolio-wallet-stats-item"]}>
+                            <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total
+                                Value</Translate>
+                            </div>
+                            <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={2230240}/>
+                            </div>
+                        </div>
+                        <div className={styles["portfolio-wallet-stats-item"]}>
+                            <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Native
+                                Value</Translate></div>
+                            <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={1230240}/>
+                            </div>
+                        </div>
+                        <div className={styles["portfolio-wallet-stats-item"]}>
+                            <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Stable
+                                Value</Translate></div>
+                            <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={230240}/>
+                            </div>
+                        </div>
+                        <div className={styles["portfolio-wallet-stats-item"]}>
+                            <div className={styles["portfolio-wallet-stats-item-title"]}><Translate>Total Token
+                                Value</Translate></div>
+                            <div className={styles["portfolio-wallet-stats-item-amount"]}><Amount amount={730240}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th scope="col"><Translate>Token</Translate></th>
-                        <th scope="col"><Translate>Type</Translate></th>
-                        <th scope="col"><Translate>Balance</Translate></th>
-                        <th scope="col"><Translate>Price</Translate></th>
-                            <th scope="col"><Translate>USD Value</Translate></th>
-                            <th scope="col"><Translate>Options</Translate></th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <div class={styles['portfolio-section']} >
+                    <div className={styles['portfolio-wallet-section-title']}><Translate>Assets</Translate></div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col"><Translate>Token</Translate></th>
+                                <th scope="col"><Translate>Type</Translate></th>
+                                <th scope="col"><Translate>Price</Translate></th>
+                                <th scope="col"><Translate>Balance</Translate></th>
+                                <th scope="col"><Translate>USD Value</Translate></th>
+                                <th scope="col"><Translate>Options</Translate></th>
+                            </tr>
+                        </thead>
+                        <tbody>
                         {
                             tokens.map((token, index) => (
-                                <tr key={index} className={styles["token-row"]} >
+                                <tr key={index} className={styles["token-row"]}>
                                     <td scope="row">
                                         <div className={styles['token-details-container']}>
-                                            <img src="/svg/icons/base-logo.svg" alt="Token Logo" width={25}/>
+                                            <div className={styles['token-image-container']}>
+                                                <img src={token.icon} alt="Token Logo" width={25}
+                                                     className={styles['token-image']}/>
+                                                <span className={styles['token-network-image-container']}>
+                                                    <img src="/svg/icons/base-logo.svg" alt="Token Logo" width={25}/>
+                                                </span>
+                                            </div>
                                             <span>ETH</span>
                                         </div>
                                     </td>
@@ -86,19 +127,20 @@ export default function PortfolioWallet({ }) {
                                         <span
                                             className={styles['token-type'] + ' ' + styles['type-' + token.type]}>Native</span>
                                     </td>
-                                    <td><Amount amount={token.price} /></td>
+                                    <td><Amount amount={token.price}/></td>
                                     <td>{token.balance}</td>
-                                    <td><Amount amount={token.price * token.balance} /></td>
+                                    <td><Amount amount={token.price * token.balance}/></td>
                                     <td>
-                                        <button title={t('Transactions')} >
-                                            <ChartIcon />
+                                        <button title={t('Transactions')}>
+                                            <ChartIcon/>
                                         </button>
                                     </td>
                                 </tr>
                             ))
                         }
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
